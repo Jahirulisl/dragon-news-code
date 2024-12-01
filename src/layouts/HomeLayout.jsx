31d0c0a9da2c02d56389ffd332304aa91c0631b8
jahir <nockjahirul@gmail.com>
2024-12-02 00:00:21 +0600

    add navbar section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,13 +1,17 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
-       <div>
+       <div class="font-poppins">
         <header>
             <Header></Header>
             <section className="w-11/12 mx-auto">
                 <LatestNews></LatestNews>
+            </section>
+            <section className="w-11/12 mx-auto py-2">
+                <Navbar></Navbar>
             </section>
         </header>
         <nav></nav>
